7b8cc08bac006c2e876ac39e972abad7fc387fc3
EzEmbedded <EzEmbedded@users.noreply.github.com>
2021-10-11 13:10:56 +0800

    Update poky-project-fetch-src.sh

--- scripts/poky-project-fetch-src.sh
+++ scripts/poky-project-fetch-src.sh
@@ -35,7 +35,7 @@ sed -i '$a\PACKAGECONFIG_append_pn-chromium = " component-build"'  conf/local.co
 sed -i '$a\PACKAGECONFIG_append_pn-chromium = " proprietary-codecs"'  conf/local.conf
 sed -i '$a\PACKAGECONFIG_append_pn-chromium = " use-vaapi"'  conf/local.conf
 sed -i '$a\INHERIT += "rm_work"'  conf/local.conf
-sed -i '$a\IMAGE_INSTALL_append = " chromium-x11"'  conf/local.conf
+# sed -i '$a\IMAGE_INSTALL_append = " chromium-x11"'  conf/local.conf
 
 
 
