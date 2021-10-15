e338c362aaa0d915d3fb3229db3e32544b92634a
EzEmbedded <EzEmbedded@users.noreply.github.com>
2021-10-15 13:03:22 +0800

    Update poky-fetch-src.sh

--- scripts/poky-fetch-src.sh
+++ scripts/poky-fetch-src.sh
@@ -5,7 +5,7 @@ git clone  git://git.openembedded.org/meta-openembedded -b hardknott
 git  clone git://github.com/kraj/meta-clang -b hardknott 
 git clone git://git.openembedded.org/meta-python2 -b hardknott 
 git clone https://github.com/OSSystems/meta-browser.git -b master
-git clone https://github.com/koenkooi/meta-kodi.git -b hardknott
+git clone https://github.com/EzEmbedded/meta-kodi.git -b hardknott
 # git clone -b gatesgarth git://git.openembedded.org/meta-openembedded
 # git clone -b gatesgarth git://git.yoctoproject.org/meta-raspberrypi
 # git clone -b gatesgarth https://github.com/meta-qt5/meta-qt5
