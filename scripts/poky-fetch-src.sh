f96c840d01ec36791db207c273fea3f5f1de42dd
EzEmbedded <EzEmbedded@users.noreply.github.com>
2021-10-25 13:12:24 +0800

    Update poky-fetch-src.sh

--- scripts/poky-fetch-src.sh
+++ scripts/poky-fetch-src.sh
@@ -40,6 +40,7 @@ bitbake-layers add-layer ../meta-browser/meta-chromium/
 sed -i '/^MACHINE/s/= .*$/= "khadas-vim3l-sdboot"/g' conf/local.conf
 sed -i '/^#SDKMA/s/#\(.*\)=.*$/\1= "x86_64"/g'  conf/local.conf
 sed -i '$a\BB_GENERATE_MIRROR_TARBALLS = "1"'  conf/local.conf
+sed -i '$a\DISTRO_FEATURES_append = "pam"'  conf/local.conf    # cockpit
 sed -i '$a\INHERIT += "buildhistory"'  conf/local.conf
 sed -i '$a\BUILDHISTORY_COMMIT = "1"'  conf/local.conf
 # sed -i '$a\BB_NUMBER_THREADS = "2"'  conf/local.conf
