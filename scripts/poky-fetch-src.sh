fa32cbfc9610e8a19ce5d29310b6d20c62df37cf
EzEmbedded <EzEmbedded@users.noreply.github.com>
2021-10-26 09:29:10 +0800

    Update poky-fetch-src.sh

--- scripts/poky-fetch-src.sh
+++ scripts/poky-fetch-src.sh
@@ -77,4 +77,4 @@ cat conf/bblayers.conf
 # bitbake amlogic-image-headless-sd -c populate_sdk_ext --runonly=fetch
 bitbake openwrt-image-full  -c populate_sdk_ext --runonly=fetch #openwrt-image-base/openwrt-image-full
 # bitbake amlogic-image-sato -c populate_sdk_ext --runonly=fetch
-# buildhistory-collect-srcrevs -a > collect-srcrevs.txt
+buildhistory-collect-srcrevs -a > collect-srcrevs.txt
