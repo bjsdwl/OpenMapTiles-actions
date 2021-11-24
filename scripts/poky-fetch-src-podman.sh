3c50df4b6cf21ca44536f7148341a7ba7911b744
EzEmbedded <EzEmbedded@users.noreply.github.com>
2021-11-24 17:29:56 +0800

    Update poky-fetch-src-podman.sh

--- scripts/poky-fetch-src-podman.sh
+++ scripts/poky-fetch-src-podman.sh
@@ -21,7 +21,7 @@ pwd
 bitbake-layers add-layer ../meta-openembedded/meta-oe/
 bitbake-layers add-layer ../meta-openembedded/meta-python/
 bitbake-layers add-layer ../meta-openembedded/meta-perl/
-bitbake-layers add-layer ../meta-openembedded/meta-multimedia/
+# bitbake-layers add-layer ../meta-openembedded/meta-multimedia/
 bitbake-layers add-layer ../meta-openembedded/meta-networking/
 bitbake-layers add-layer ../meta-openembedded/meta-filesystems/	
 # bitbake-layers add-layer ../meta-openembedded/meta-webserver/	
