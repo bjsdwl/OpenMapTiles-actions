dede936676525706c5369878818d94ab7e803038
EzEmbedded <EzEmbedded@users.noreply.github.com>
2021-11-26 09:16:48 +0800

    Update poky-fetch-src-podman.sh

--- scripts/poky-fetch-src-podman.sh
+++ scripts/poky-fetch-src-podman.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-git clone --depth 1 git://git.yoctoproject.org/poky -b honister  poky
-git clone https://github.com/superna9999/meta-meson.git meta-meson -b honister 
-git clone git://git.openembedded.org/meta-openembedded -b honister 
-git clone git://git.yoctoproject.org/meta-virtualization -b honister 
+git clone --depth 1 git://git.yoctoproject.org/poky -b thud  poky
+git clone https://github.com/superna9999/meta-meson.git meta-meson -b thud 
+git clone git://git.openembedded.org/meta-openembedded -b thud 
+git clone git://git.yoctoproject.org/meta-virtualization -b thud 
 # git clone git://git.openembedded.org/meta-perl -b hardknott
-git clone git://git.yoctoproject.org/meta-security -b honister 
-git clone git://github.com/kraj/meta-clang -b honister  
+git clone git://git.yoctoproject.org/meta-security -b thud 
+git clone git://github.com/kraj/meta-clang -b thud  
 # git clone git://git.openembedded.org/meta-python2 -b thud  
 # git clone https://github.com/OSSystems/meta-browser.git -b thud 
 # git clone https://github.com/EzEmbedded/meta-kodi.git -b thud 
