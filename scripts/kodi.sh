e234b19dace9aec0cfc92d4522c39dc4f1e3de96
EzEmbedded <EzEmbedded@users.noreply.github.com>
2021-12-01 13:23:43 +0800

    yy

--- scripts/kodi.sh
+++ scripts/kodi.sh
@@ -80,3 +80,4 @@ cat conf/bblayers.conf
 # bitbake amlogic-image-headless-sd -c populate_sdk_ext --runonly=fetch
 # bitbake openwrt-image-full  -c populate_sdk_ext --runonly=fetch #openwrt-image-base/openwrt-image-full
 bitbake amlogic-image-headless-initrd  --runonly=fetch
+buildhistory-collect-srcrevs -a > collect-srcrevs.txt
