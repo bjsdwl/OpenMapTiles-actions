1deb2386182de36b34c1344f088fce09125f61ea
EzEmbedded <EzEmbedded@users.noreply.github.com>
2022-01-25 10:58:14 +0800

    Update poky-fetch-src-podman.sh

--- scripts/poky-fetch-src-podman.sh
+++ scripts/poky-fetch-src-podman.sh
@@ -53,7 +53,7 @@ sed -i '$a\PACKAGE_CLASSES ?= "package_ipk"'  conf/local.conf
 
 # podman: cockpit podman podman-compose crun cgroup-lite rng-tools procps ca-certificates python3-setuptools python3-pyyaml python3-json
 # docker-ce: cockpit docker-ce docker-ce-contrib python3 python3-docker-compose
-sed -i '$a\IMAGE_INSTALL_append += " kodi gstreamer1.0-plugins-good ffmpeg chromium-x11 x11vnc vlc mpv cockpit podman podman-compose crun cgroup-lite rng-tools procps ca-certificates python3-setuptools python3-pyyaml python3-json docker-ce docker-ce-contrib python3 python3-docker-compose"'  conf/local.conf
+# sed -i '$a\IMAGE_INSTALL_append += " kodi gstreamer1.0-plugins-good ffmpeg chromium-x11 x11vnc vlc mpv cockpit podman podman-compose crun cgroup-lite rng-tools procps ca-certificates python3-setuptools python3-pyyaml python3-json docker-ce docker-ce-contrib python3 python3-docker-compose"'  conf/local.conf
 
 # cat /home/yocto-crops/Zdownloads/collect-srcrevs.txt >> conf/local.conf
 
@@ -73,5 +73,5 @@ cat conf/bblayers.conf
 # bitbake amlogic-image-headless-sd  --runonly=fetch 
 # bitbake amlogic-image-headless-sd -c populate_sdk_ext --runonly=fetch
 # bitbake openwrt-image-full  -c populate_sdk_ext --runonly=fetch #openwrt-image-base/openwrt-image-full
-bitbake amlogic-image-sato -c populate_sdk_ext  --runonly=fetch
+bitbake chromium # amlogic-image-sato -c populate_sdk_ext  --runonly=fetch
 buildhistory-collect-srcrevs -a > collect-srcrevs.txt
