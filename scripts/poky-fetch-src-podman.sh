acdf22370dc57e79c57165f0e91fe38dd58c31d3
EzEmbedded <EzEmbedded@users.noreply.github.com>
2022-01-25 16:12:03 +0800

    Update poky-fetch-src-podman.sh

--- scripts/poky-fetch-src-podman.sh
+++ scripts/poky-fetch-src-podman.sh
@@ -17,7 +17,7 @@ cat mypatch/docker-ce.patch
 cd meta-virtualization
 pwd
 
-git apply ../mypatch/curn_git.patch
+git apply ../../mypatch/curn_git.patch
 git apply ../mypatch/docker-ce.patch
 
 cat recipes-containers/crun/crun_git.bb
@@ -63,7 +63,7 @@ sed -i '$a\LICENSE_FLAGS_WHITELIST += " commercial_mpg123 commercial commercial_
 # systemd pam procd 
 sed -i '$a\DISTRO_FEATURES_append = " virtualization systemd pam procd"'  conf/local.conf # cockpit---pam  podman---virtualization podman---systemd
 sed -i '$a\PACKAGE_CLASSES ?= "package_ipk"'  conf/local.conf
-# sed -i '$a\INHERIT += "rm_work"'  conf/local.conf
+sed -i '$a\INHERIT += "rm_work"'  conf/local.conf                ############################        
 
 # podman: cockpit podman podman-compose crun cgroup-lite rng-tools procps ca-certificates python3-setuptools python3-pyyaml python3-json
 # docker-ce: cockpit docker-ce docker-ce-contrib python3 python3-docker-compose
