109ba6c468bf5050234a8adc91c1692a727cd19c
EzEmbedded <EzEmbedded@users.noreply.github.com>
2022-01-27 13:56:14 +0800

    Update poky-fetch-src-podman.sh

--- scripts/poky-fetch-src-podman.sh
+++ scripts/poky-fetch-src-podman.sh
@@ -10,20 +10,12 @@ git clone https://github.com/OSSystems/meta-browser.git -b master
 git clone https://github.com/koenkooi/meta-kodi.git -b master-next 
 git clone git://github.com/kraj/meta-openwrt.git -b hardknott 
 cd meta-virtualization
-echo "check recipes-containers.patch"
-cat ../mypatch/recipes-containers.patch
 echo "apply recipes-containers.patch"
 git apply ../mypatch/recipes-containers.patch
-
-
-cat recipes-containers/crun/crun_git.bb
-cat recipes-containers/docker/docker-ce_git.bb
 cd ../
-
 pwd 
-ls -l
+
 source poky/oe-init-build-env # in build dir
-pwd
 
 bitbake-layers add-layer ../meta-openembedded/meta-oe/
 bitbake-layers add-layer ../meta-openembedded/meta-python/
