8eca3ccde7d36b87e20bc188baa22b516ade0693
EzEmbedded <EzEmbedded@users.noreply.github.com>
2022-01-28 15:06:01 +0800

    bitbake build multiple targets

--- scripts/poky-fetch-src-podman.sh
+++ scripts/poky-fetch-src-podman.sh
@@ -79,5 +79,5 @@ cat conf/bblayers.conf
 # bitbake amlogic-image-headless-sd  --runonly=fetch 
 # bitbake amlogic-image-headless-sd -c populate_sdk_ext --runonly=fetch
 # bitbake openwrt-image-full  -c populate_sdk_ext --runonly=fetch #openwrt-image-base/openwrt-image-full
-bitbake  python3-docker-compose # amlogic-image-sato -c populate_sdk_ext  --runonly=fetch
+bitbake  docker-ce-contrib && bitbake python3 && bitbake python3-docker-compose # amlogic-image-sato -c populate_sdk_ext  --runonly=fetch
 buildhistory-collect-srcrevs -a > collect-srcrevs.txt
