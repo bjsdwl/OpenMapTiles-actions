5e84951856b2c4837846c4dae69ba0b62a31b530
EzEmbedded <EzEmbedded@users.noreply.github.com>
2022-11-24 11:14:40 +0800

    Update poky-fetch-src-podman.sh

--- scripts/poky-fetch-src-podman.sh
+++ scripts/poky-fetch-src-podman.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-git clone https://github.com/openmaptiles/openmaptiles.git -b master
-cd OpenMapTiles
+# git clone https://github.com/openmaptiles/openmaptiles.git -b master
+git clone https://github.com/OSSystems/meta-browser.git -b master
+cd OSSystems
 make
 make start-db
 make download area=albania
